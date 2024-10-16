99651964d37189efc7b1395c2f00b9c6f8d73e9b
Jack Mwisa <87465845+JackMwisa@users.noreply.github.com>
2024-10-16 23:42:47 +0300

    Update outsideJob.java

--- src/outsideJob.java
+++ src/outsideJob.java
@@ -1,23 +1,58 @@
 import java.util.ArrayList;
 import java.util.List;
 
-public abstract class outsideJob implements Job {
-
+public class OutsideJob implements Job {
     protected double salary;
     protected String startShift;
     protected String endShift;
     protected String jobDescription;
     protected List<Person> workers;
 
-
-    public outsideJob(double salary, String startShift, String endShift, String jobDescription) {
+    public OutsideJob(double salary, String startShift, String endShift, String jobDescription) {
         this.salary = salary;
         this.startShift = startShift;
         this.endShift = endShift;
         this.jobDescription = jobDescription;
-        this.workers = new ArrayList<Person>();
+        this.workers = new ArrayList<>();
     }
 
+    @Override
+    public double salary() {
+        return salary;
+    }
 
-    
+    @Override
+    public String startShift() {
+        return startShift;
+    }
+
+    @Override
+    public String endShift() {
+        return endShift;
+    }
+
+    @Override
+    public void printJobDescription() {
+        System.out.println("Job Description: " + jobDescription);
+    }
+
+    @Override
+    public void printWorkers() {
+        System.out.println("Our workers in this job are:");
+        for (Person person : workers) {
+            System.out.println("Name: " + person.getName());
+        }
+    }
+
+    @Override
+    public void hirePerson(Person person) {
+        workers.add(person);
+        System.out.println(person.getName() + " has been hired.");
+    }
+
+    @Override
+    public void firePerson(Person person) {
+        workers.remove(person);
+        System.out.println(person.getName() + " has been fired.");
+    }
 }
